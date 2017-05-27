814957327542fda03ca0d825bc3e6e14ad278286
helios1138 <revan.den@gmail.com>
2017-05-27 21:46:52 +0300

    timesheet/weeks reducer refactoring and improvement

--- src/reducers/timesheet/weeks.js
+++ src/reducers/timesheet/weeks.js
@@ -1,23 +1,48 @@
-import { handleActions } from 'redux-actions'
+import { handleActions, combineActions } from 'redux-actions'
+import _ from 'lodash'
 
 import { app } from '../../actions/app'
 
+const handleWeek = handleActions(
+  {
+    [combineActions(
+      app.timesheet.load,
+      app.timesheet.approve,
+      app.timesheet.reject
+    )]: (state, { payload: week }) => ({
+      status: week.status,
+      approvedBy: week.approved_by_id,
+      approvedAt: new Date(week.approved_by_date),
+      days: _.map(week.days_in_week, 'id')
+    })
+  },
+  {
+    status: null,
+    approvedBy: null,
+    approvedAt: null,
+    days: []
+  }
+)
+
 export const weeks = handleActions({
-  [app.timesheet.load]: (state, { payload: { data: { weeks } } }) => {
+  [app.timesheet.load]: (state, { type, payload: { data: { weeks } } }) => {
     const update = {}
 
-    weeks.forEach(week =>
-      update[week.week_id] = {
-        status: week.status,
-        approvedBy: week.approved_by_id,
-        approvedAt: new Date(week.approved_by_date),
-        days: week.days_in_week.map(day => day.id)
-      }
-    )
+    weeks.forEach(week => {
+      update[week.week_id] = handleWeek(update[week.week_id], { type, payload: week })
+    })
 
     return {
       ...state,
       ...update
     }
-  }
+  },
+
+  [combineActions(
+    app.timesheet.approve,
+    app.timesheet.reject
+  )]: (state, { type, payload: week }) => ({
+    ...state,
+    [week.week_id]: handleWeek(state[week.week_id], { type, payload: week })
+  })
 }, {})
